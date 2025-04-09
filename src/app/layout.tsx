b28de1a57a0dc6da297df8d74f6cdd167aa26480
UsmanRizan <usmanrizan6@gmail.com>
2025-04-09 23:45:20 +0530

    Build Sidebar II

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 export const metadata: Metadata = {
   title: "Goat Notes",
@@ -25,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SidebarProvider>
+            <AppSidebar />
             <div className="flex min-h-screen w-full flex-col">
               <Header />
               <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
